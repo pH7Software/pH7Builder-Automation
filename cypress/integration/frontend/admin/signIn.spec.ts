f33f4923fd02bac39f6e7c05818f516b9a6c15fc
Pierre-Henry Soria <hi@ph7.me>
2022-04-02 15:21:37 +1030

    Add admin integration tests

---
+++ cypress/integration/frontend/admin/signIn.spec.ts
@@ -0,0 +1,18 @@
+describe("signIn", () => {
+  beforeEach(() => {
+    cy.visit("admin123");
+  });
+
+  describe("errors", () => {
+    it(`must receive an error message when credentials aren't valid`, () => {
+      cy.get("[name=username]").type("invalid username");
+      cy.get("[name=mail]").type("doesntexist@ph7builder.com");
+      cy.get("[name=password]").type("invalid password");
+      cy.get("form").submit();
+
+      // Check error messages
+      cy.get("form").should("contain.text", "is incorrect");
+      cy.get("form").should("have.class", "pfbc-error");
+    });
+  });
+});
