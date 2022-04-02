693814e503600f297dd807a9a77fbccd76676748
Pierre-Henry Soria <hi@ph7.me>
2022-04-02 15:21:12 +1030

    [Prettier|ESLint] Cleanup/Indent code

--- cypress/integration/frontend/user/signIn.spec.ts
+++ cypress/integration/frontend/user/signIn.spec.ts
@@ -1,32 +1,38 @@
-import faker from 'faker';
-import RegistrationUser from '@lib/user/user.data';
+import faker from "faker";
+import RegistrationUser from "@lib/user/user.data";
 
-describe('signIn', () => {
-  const formElement = 'form#form_login_user';
+describe("signIn", () => {
+  const formElement = "form#form_login_user";
 
   beforeEach(() => {
-    cy.visit('login');
+    cy.visit("login");
   });
 
-  describe('errors', () => {
+  describe("errors", () => {
     it(`must receive an error message when credentials aren't valid`, () => {
-      cy.get('[name=mail]').type('doesntexist@ph7builder.com');
-      cy.get('[name=password]').type('invalid password');
+      cy.get("[name=mail]").type("doesntexist@ph7builder.com");
+      cy.get("[name=password]").type("invalid password");
       cy.get(formElement).submit();
 
       // Check error messages
-      cy.get(formElement).should('contain.text', 'The following error was found');
-      cy.get(formElement).should('have.class', 'pfbc-error');
+      cy.get(formElement).should(
+        "contain.text",
+        "The following error was found"
+      );
+      cy.get(formElement).should("have.class", "pfbc-error");
     });
 
     it(`must receive an error when email is invalid`, () => {
-      cy.get('[name=mail]').type('not an email');
-      cy.get(formElement).should('have.class', 'input_error');
-      cy.get('.input_error .email_login').contains('invalid', {matchCase: false, timeout: 1500});
+      cy.get("[name=mail]").type("not an email");
+      cy.get(formElement).should("have.class", "input_error");
+      cy.get(".input_error .email_login").contains("invalid", {
+        matchCase: false,
+        timeout: 1500,
+      });
     });
   });
 
-  describe('success', () => {
+  describe("success", () => {
     beforeEach(() => {
       createUser(registeredUser);
     });
@@ -35,31 +41,33 @@ describe('signIn', () => {
       email: faker.internet.email(),
       password: faker.internet.password(),
       firstName: faker.name.firstName(),
-      nickname: faker.internet.userName()
+      nickname: faker.internet.userName(),
     };
 
     const createUser = (user: RegistrationUser) => {
-      cy.visit('signup');
+      cy.visit("signup");
 
       // Minimum step to create a user
-      cy.get('[name=first_name]').type(user.firstName);
-      cy.get('[name=username]').type(user.nickname);
-      cy.get('[name=mail]').type(user.email);
-      cy.get('[name=password]').type(user.password);
+      cy.get("[name=first_name]").type(user.firstName);
+      cy.get("[name=username]").type(user.nickname);
+      cy.get("[name=mail]").type(user.email);
+      cy.get("[name=password]").type(user.password);
       cy.get('[name="terms[]"]').check();
-      cy.get('form#form_join_user').submit();
+      cy.get("form#form_join_user").submit();
     };
 
-    it('must sign-in as a user', () => {
-      cy.get('[name=mail]').type(registeredUser.email);
-      cy.get('[name=password]').type(registeredUser.password);
+    it("must sign-in as a user", () => {
+      cy.get("[name=mail]").type(registeredUser.email);
+      cy.get("[name=password]").type(registeredUser.password);
       cy.get(formElement).submit();
 
       // Check expected redirection
-      cy.location().should('eq', 'user/account/index', {timeout: 2000});
+      cy.location().should("eq", "user/account/index", { timeout: 2000 });
 
       // Check success message
-      cy.get('.alert .alert-success').should('be.visible').should('contain.text', 'You are successfully logged in');
+      cy.get(".alert .alert-success")
+        .should("be.visible")
+        .should("contain.text", "You are successfully logged in");
     });
   });
-})
+});
--- cypress/integration/frontend/user/signUp.spec.ts
+++ cypress/integration/frontend/user/signUp.spec.ts
@@ -1,57 +1,60 @@
-import faker from 'faker';
+import faker from "faker";
 
-describe('signUp', () => {
-  const formStep1 = 'form#form_join_user';
-  const formStep2 = 'form#form_join_user2';
-  const formStep3 = 'form#form_join_user3';
-  const formStep4 = 'form#form_join_user4';
+describe("signUp", () => {
+  const formStep1 = "form#form_join_user";
+  const formStep2 = "form#form_join_user2";
+  const formStep3 = "form#form_join_user3";
+  const formStep4 = "form#form_join_user4";
 
   beforeEach(() => {
-    cy.visit('signup');
+    cy.visit("signup");
   });
 
-  describe('success', () => {
-    it('must sign-up a user on every registration step', () => {
+  describe("success", () => {
+    it("must sign-up a user on every registration step", () => {
       // Step 1
-      cy.get('#headings').should('contain.text', 'Step 2/3');
+      cy.get("#headings").should("contain.text", "Step 2/3");
 
-      cy.get('[name=first_name]').type(faker.name.firstName());
-      cy.get('[name=username]').type(faker.internet.userName());
-      cy.get('[name=mail]').type(faker.internet.email());
-      cy.get('[name=password]').type(faker.internet.password());
+      cy.get("[name=first_name]").type(faker.name.firstName());
+      cy.get("[name=username]").type(faker.internet.userName());
+      cy.get("[name=mail]").type(faker.internet.email());
+      cy.get("[name=password]").type(faker.internet.password());
       cy.get('[name="terms[]"]').check();
       cy.get(formStep1).submit();
 
       // Step 2
-      cy.get('#headings').should('contain.text', 'Step 2/3');
+      cy.get("#headings").should("contain.text", "Step 2/3");
 
-      cy.get('[name=sex]').check('male');
-      cy.get('[name=match_sex[]]').check(['female', 'male', 'couple']);
-      cy.get('[name=birth_date]').type('2/12/2002');
-      cy.get('[name=country]').type(faker.address.countryCode());
-      cy.get('[name=city]').type(faker.address.city());
-      cy.get('[name=zip_code]').type(faker.address.zipCode());
+      cy.get("[name=sex]").check("male");
+      cy.get("[name=match_sex[]]").check(["female", "male", "couple"]);
+      cy.get("[name=birth_date]").type("2/12/2002");
+      cy.get("[name=country]").type(faker.address.countryCode());
+      cy.get("[name=city]").type(faker.address.city());
+      cy.get("[name=zip_code]").type(faker.address.zipCode());
       cy.get(formStep2).submit();
 
       // Step 3
       const userDescription = faker.lorem.sentence();
-      cy.get('#headings').should('contain.text', 'Step 3/3');
-      cy.get('[name=description]').type(userDescription);
-      cy.get(`${formStep3} p span#str_description_rem_len`).should('contain.text', userDescription.length() + ' character(s)');
+      cy.get("#headings").should("contain.text", "Step 3/3");
+      cy.get("[name=description]").type(userDescription);
+      cy.get(`${formStep3} p span#str_description_rem_len`).should(
+        "contain.text",
+        userDescription.length() + " character(s)"
+      );
       cy.get(formStep3).submit();
 
       // Step 4
-      const fixtureFilename = 'avatar.jpg';
-      cy.get('[name=avatar]').attachFile(fixtureFilename);
+      const fixtureFilename = "avatar.jpg";
+      cy.get("[name=avatar]").attachFile(fixtureFilename);
       cy.get(formStep4).submit();
 
       // Check expected redirection
-      cy.location().should('eq', 'user/signup/done', {timeout: 2000});
+      cy.location().should("eq", "user/signup/done", { timeout: 2000 });
 
       // Check success message
-      cy.get('.alert .alert-success')
-        .should('be.visible')
-        .should('contain.text', 'Your account has just been created');
+      cy.get(".alert .alert-success")
+        .should("be.visible")
+        .should("contain.text", "Your account has just been created");
     });
   });
 });
